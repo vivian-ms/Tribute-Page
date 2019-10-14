556c6d2a2dd510353dfc86e4bd4c835f9185823a
vivian-ms <vivian.ms513@gmail.com>
2019-10-13 23:16:50 -0700

    Added timeline section with card flip animation for each date

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(function() {
-  
+  // Event listeners for li elements of timeline list
+  $('.timeline_list').on({
+    // On mouseenter / touchstart, hide timeline date and show details
+    'mouseenter touchstart': function(evt) {
+      evt.preventDefault();
+      $(this).children('.date').addClass('hide').removeClass('show');
+      $(this).children('.details').addClass('show').removeClass('hide');
+
+    },
+
+    // On mouseleave / touchend, show timeline date and hide details
+    'mouseleave touchend': function(evt) {
+      evt.preventDefault();
+      $(this).children('.date').addClass('show').removeClass('hide');
+      $(this).children('.details').addClass('hide').removeClass('show');
+    }
+  }, 'li');
 });
