51a2fdd76a6f373e32d53b6450f936ec59f78c00
vivian-ms <vivian.ms513@gmail.com>
2019-10-14 20:43:08 -0700

    Improved timeline cards and changed section heading style

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-$(function() {  
-  // Event listeners for li elements of timeline list
-  $('.timeline_list').on({
-    // On mouseenter / touchstart, hide timeline date and show details
+$(function() {
+  $('.flip-card').on({
     'mouseenter touchstart': function(evt) {
       evt.preventDefault();
-      $(this).children('.date').addClass('hide').removeClass('show');
-      $(this).children('.details').addClass('show').removeClass('hide');
-
+      $(this).children('.flip-card-inner').addClass('flip-card-hover');
     },
 
-    // On mouseleave / touchend, show timeline date and hide details
     'mouseleave touchend': function(evt) {
       evt.preventDefault();
-      $(this).children('.date').addClass('show').removeClass('hide');
-      $(this).children('.details').addClass('hide').removeClass('show');
+      $(this).children('.flip-card-inner').removeClass('flip-card-hover');
     }
-  }, 'li');
+  });
 });
